a7107c72d0da6399335cb8e41b082489b3b3f953
shishi42 <benji.riviere72@hotmail.fr>
2026-06-20 01:45:10 +0200

    fix archiving

--- commands/archive.js
+++ commands/archive.js
@@ -19,23 +19,24 @@ module.exports = {
 
   async run(bot, message, args) {
 
+    await message.deferReply({ ephemeral: true })
+
     let medias = []
     let messages = args.get("messages").value.split(' ')
-    let regex = /https:\/\/discord\.com\/channels\/(\d+)\/(\d+)\/(\d+)/
 
     for (message_url of messages) {
-      match = message_url.match(regex)
-      [_, _, channelId, messageId] = match
-      channel = await message.guild.channels.fetch(channelId)
-      message = await channel.messages.fetch(messageId)
-      for (attachment of message.attachments) {
-        medias.push(attachment.url)
-        if (medias.length == 10){
-          await message.channel.send({content : "archive", files : medias})} 
+      let channel = await message.guild.channels.fetch(message_url.split('/')[5])
+      let msg = await channel.messages.fetch(message_url.split('/')[6])
+
+      for (attachment of msg.attachments) {
+        medias.push(attachment[1].url)
+        if (medias.length == 9){
+          await message.channel.send({content : "archive", files : medias})
           medias = []
         }
       }
-    return await message.reply({content: "C'est bon."})
+    }
+    await message.channel.send({ content: "archiving...", files: medias })
+    return await message.editReply({ content: "C'est bon.", ephemeral: true })
   }
-
 }
