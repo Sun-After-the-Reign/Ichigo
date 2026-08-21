c40a9742312a8ea69b81101cc72b317b16133f76
Shishi <37541779+Shishi42@users.noreply.github.com>
2026-08-21 17:43:26 +0200

    Enhance liste-clan command with participant counts
    Updated the command to include participant counts for each clan.

--- commands/liste-clan.js
+++ commands/liste-clan.js
@@ -21,17 +21,21 @@ module.exports = {
 
     await message.deferReply()
 
+    let msg = ""
+
     let tournament = await bot.Tournaments.findOne({ where: { tournament_id: args.get("tournament_id").value.split(" - ")[0] } })
 
     let requestOptions = { method: 'GET', headers: bot.myHeaders, redirect: 'follow' }
     let request = await fetch("https://api.challonge.com/v2.1/tournaments/" + tournament.dataValues.tournament_challonge + "/participants.json?community_id=sunafterthereign&per_page=200", requestOptions)
     let response = await request.json()
 
-    let participants = response.data.filter(p => p.attributes.name.includes(" | ") && p.attributes.final_rank != null)
+    let participants = response.data.filter(p => p.attributes.name.includes(" | "))
 
     let clans = Array.from(new Set(participants.map(p => p.attributes.name.split(" | ")[0]))).sort()
 
-    return await message.editReply(clans.join('\n'))
+    for (clan of clans) msg += clan + " (" + response.data.filter(p => p.attributes.name.includes(clan)).length + ")\n"
+
+    return await message.editReply(msg)
 
   }
 }
